8ae1c4dadae59236ff52b209e1ce86185a0b4276
William Villeneuve <ciiqr@users.noreply.github.com>
2022-09-02 19:33:20 -0400

    dropped eslint-config-nestjs

--- packages/eslint-config-nestjs/typeHelpers.js
+++
@@ -1,8 +0,0 @@
-// all TitleCase exports in *-type.helper.ts files from each nest package
-exports.typeHelpers = [
-    // nestjs/graphql
-    "IntersectionType",
-    "OmitType",
-    "PartialType",
-    "PickType",
-];
