c38bfd057bb93c55173701b572d1fb4a5060179b
William Villeneuve <ciiqr@users.noreply.github.com>
2023-08-22 20:59:48 -0400

    eslint-config-next: app router routes.ts support

--- packages/eslint-config-next/index.js
+++ packages/eslint-config-next/index.js
@@ -1,3 +1,5 @@
+const { naming } = require("@pentible/eslint-config/naming");
+
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
     extends: ["next/core-web-vitals"],
@@ -18,6 +20,24 @@ module.exports = {
             files: ["src/pages/_app.{js,jsx,ts,tsx}"],
             rules: {
                 "react/jsx-props-no-spreading": "off",
+            },
+        },
+        {
+            files: ["src/app/**/route.{js,ts}"],
+            rules: {
+                "@typescript-eslint/naming-convention": [
+                    "error",
+                    {
+                        format: ["UPPER_CASE"],
+                        selector: "function",
+                        modifiers: ["exported"],
+                        filter: {
+                            regex: "^(GET|POST|PUT|PATCH|DELETE|HEAD|OPTIONS)$",
+                            match: true,
+                        },
+                    },
+                    ...naming,
+                ],
             },
         },
     ],
