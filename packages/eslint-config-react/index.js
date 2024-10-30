8ddb7ddfca9510f67dc6a3d45f5f5c55813b356f
William Villeneuve <ciiqr@users.noreply.github.com>
2024-10-29 21:22:33 -0400

    no longer really care about eslint-plugin-react-prefer-function-component

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -2,7 +2,6 @@
 module.exports = {
     extends: [
         "plugin:react/recommended",
-        "plugin:react-prefer-function-component/recommended",
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/strict",
