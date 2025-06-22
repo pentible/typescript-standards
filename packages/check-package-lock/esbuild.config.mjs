9887113b9f4cb8de91bf00a538a52f8c2ad509d6
William Villeneuve <ciiqr@users.noreply.github.com>
2025-06-22 19:13:00 -0400

    compressed esbuild cjs header a bit

--- packages/check-package-lock/esbuild.config.mjs
+++ packages/check-package-lock/esbuild.config.mjs
@@ -2,21 +2,7 @@ import esbuild from "esbuild";
 
 // TODO: move this somewhere common
 // SEE: https://github.com/evanw/esbuild/issues/946#issuecomment-814703190
-const cjsCompatibilityBanner = `
-    import { createRequire as topLevelCreateRequire } from 'module';
-    import { fileURLToPath as topLevelFileURLToPath } from 'url';
-    import { dirname as topLevelDirname, join as topLevelJoin } from 'path';
-    const __bundleRequire = topLevelCreateRequire(import.meta.url);
-    const __bundleFilename = topLevelFileURLToPath(import.meta.url);
-    const __bundleDirname = topLevelDirname(__bundleFilename);
-    const __bundleJoin = topLevelJoin;
-
-    // Make these variables available globally for compatibility
-    globalThis.require = __bundleRequire;
-    globalThis.__filename = __bundleFilename;
-    globalThis.__dirname = __bundleDirname;
-    globalThis.join = __bundleJoin;
-`;
+const cjsCompatibilityBanner = `import { createRequire as topLevelCreateRequire } from 'module';import { fileURLToPath as topLevelFileURLToPath } from 'url';import { dirname as topLevelDirname, join as topLevelJoin } from 'path';const __bundleRequire = topLevelCreateRequire(import.meta.url);const __bundleFilename = topLevelFileURLToPath(import.meta.url);const __bundleDirname = topLevelDirname(__bundleFilename);const __bundleJoin = topLevelJoin;globalThis.require = __bundleRequire;globalThis.__filename = __bundleFilename;globalThis.__dirname = __bundleDirname;globalThis.join = __bundleJoin;`;
 
 /** @satisfies {import("esbuild").BuildOptions} */
 const options = {
